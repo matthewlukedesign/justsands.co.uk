6700d6dacb9133fa8dca70f7a9b38d02bce84911
matthewlukedesign <matt@matthewluke.design>
2021-10-21 16:16:25 +0100

    Fix links

--- frontend/components/layout/Footer.vue
+++ frontend/components/layout/Footer.vue
@@ -3,9 +3,12 @@
     class="relative z-10 justify-between hidden mt-auto leading-none md:flex"
   >
     <NuxtLink to="/">&copy; SANDS {{ new Date().getFullYear() }}</NuxtLink>
-    <NuxtLink :to="link[0].slug" v-for="(link, index) in menu" :key="index">{{
-      link[0].title
-    }}</NuxtLink>
+    <NuxtLink
+      :to="link.slug"
+      v-for="(link, index) in menu.links"
+      :key="index"
+      >{{ link.title }}</NuxtLink
+    >
   </header>
 </template>
 <script>
