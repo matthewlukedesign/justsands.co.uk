dafa1ada3dd75c486efabc190a1c6efff3074156
matthewlukedesign <matt@matthewluke.design>
2021-11-17 11:24:40 +0000

    Invert colours

--- frontend/components/svg/Cross.vue
+++ frontend/components/svg/Cross.vue
@@ -1,11 +1,11 @@
 <template>
   <svg
-    class="w-full h-auto"
+    class="w-full h-auto stroke-current"
     viewBox="0 0 62 62"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M1 1L61 61" stroke-width="2" stroke="black" />
-    <path d="M61 1L0.999997 61" stroke-width="2" stroke="black" />
+    <path d="M1 1L61 61" stroke-width="2" />
+    <path d="M61 1L0.999997 61" stroke-width="2" />
   </svg>
 </template>
